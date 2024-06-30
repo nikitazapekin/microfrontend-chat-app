2737a41892fc961905f3a1987da37ad83754172a
nikitazapekin <wotblitz362@mail.ru>
2024-06-30 11:09:08 +0300

    get messages

--- node_modules/shop/src/components/ChatMain/index.tsx
+++ node_modules/shop/src/components/ChatMain/index.tsx
@@ -1,99 +1,4 @@
-/*
-
-import ChatWithUser from "../ChatWithUser";
-import ListOfUsers from "../ListOfUsers";
-import SessionModal from "../SessionModal";
-import styles from "./index.module.scss";
-import { useAppDispatch } from '@/hooks/redux';
-import { useEffect, useState } from "react";
-import { IsUnauthorizedAction } from "@packages/shared/store/action-creators/IsAuthorizedActionCreator";
-import axios from "axios";
-import { useSelector } from 'react-redux';
-import { authSelector } from "@packages/shared/store/selectors/auth.selector";
-import { selectedChatSelector } from "@packages/shared/store/selectors/selectedChat.selector";
-import EmojiWhileHovering from "../EmojiWhileHovering";
-const ChatMain = () => {
-    const authData = useSelector(authSelector);
-    const isSelected = useSelector(selectedChatSelector)
-    const dispatch = useAppDispatch();
-    const URL_WEB_SOCKET = 'ws://127.0.0.1:5000/ws';
-    const [ws, setWs] = useState(null);
-    const [message, setMessage] = useState("");
-    useEffect(() => {
-        let socket = new WebSocket(URL_WEB_SOCKET);
-        setWs(socket);
-        console.log("Attempting Connection...");
-        socket.onopen = () => {
-            console.log("Successfully Connected");
-            socket.send(JSON.stringify({ connectedUser: authData.username }))
-        };
-        socket.onclose = event => {
-            console.log("Socket Closed Connection: ", event);
-            socket.send("Client Closed!");
-        };
-        socket.onerror = error => {
-            console.log("Socket Error: ", error);
-        }
-        return () => {
-            socket.close();
-        };
-    }, []);
-    useEffect(() => {
-        if (!localStorage.getItem('token')) {
-            dispatch(IsUnauthorizedAction({ isUnauthorized: false }));
-        }
-    }, [dispatch]);
-    useEffect(() => {
-        axios.get(`http://localhost:5000/${`chat`}/token-start?user=${authData.username}`)
-            .then(response => {
-                console.log('Ответ от сервера TOKEN START:', response.data);
-            })
-            .catch(error => {
-                console.error('Произошла ошибка при запросе:', error);
-            });
-    }, [authData.username]);
-    const handleSendMessage = () => {
-        if (ws && ws.readyState === WebSocket.OPEN) {
-
-            console.log("SENDDDD")
-        //    ws.send(JSON.stringify({ name: authData.username, message: message, to: isSelected.user }));
-        ws.send(JSON.stringify({ name: "authData.username", message:" message", to: "isSelected.user" }));
-            setMessage("");
-        }
-    };
-    const handleSetMessage = (value: string) => {
-        setMessage(value)
-    }
-    return (
-        <>
-            <div className={styles.container}>
-                <ListOfUsers />
-                <ChatWithUser handleSendMessage={handleSendMessage} handleSetMessage={handleSetMessage} />
-                <SessionModal />
-                <div className={styles.chatInputContainer}>
-                    <input
-                        type="text"
-                        value={message}
-                        onChange={(e) => handleSetMessage(e.target.value)}
-                        placeholder="Type your message..."
-                        className={styles.chatInput}
-                    />
-                    <button onClick={handleSendMessage} className={styles.sendButton}>
-                        Send
-                    </button>
-                </div>
-            </div>
-
-           
-            </>
-    );
-};
-
-//<EmojiWhileHovering />
-export default ChatMain;
-
-*/
-
+ 
 
 import ChatWithUser from "../ChatWithUser";
 import ListOfUsers from "../ListOfUsers";
@@ -155,10 +60,8 @@ const ChatMain = () => {
     }, [authData.username]);
     const handleSendMessage = () => {
         if (ws && ws.readyState === WebSocket.OPEN) {
-
             console.log("SENDDDD")
             ws.send(JSON.stringify({ name: authData.username, message: message, to: isSelected.user }));
-            //   ws.send(JSON.stringify({ name: "authData.username", message:" message", to: "isSelected.user" }));
             setMessage("");
         }
     };
--- services/shop/src/components/ChatWithUser/index.tsx
+++ services/shop/src/components/ChatWithUser/index.tsx
@@ -4,26 +4,76 @@ import ChatKeypad from "../ChatKeypad"
 import { useEffect } from "react"
 import { useSelector } from 'react-redux';
 import { selectedChatSelector } from "@packages/shared/store/selectors/selectedChat.selector"
+import { authSelector } from "@packages/shared/store/selectors/auth.selector";
+import { useState} from "react";
+ 
 interface ChatWithUserProps {
     handleSendMessage: () => void;
     handleSetMessage: (message: string) => void;
 }
+
+
+
+interface MessageData {
+    from: string;
+    to: string;
+    message: string;
+    time: string;  
+}
+
+
+
 const ChatWithUser = ({handleSendMessage, handleSetMessage}:  ChatWithUserProps) => {
     const isSelected = useSelector(selectedChatSelector)
+    const authData = useSelector(authSelector);
+    
+    const URL_WEB_SOCKET = `ws://localhost:5000/ws/chat?user=${authData.username}&companion=${isSelected.user}`;
+    const [ws, setWs] = useState(null);
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState<MessageData[]>([])
+    useEffect(() => {
+        let socket = new WebSocket(URL_WEB_SOCKET);
+        console.log("SOCKET", JSON.stringify(socket))
+        setWs(socket);
+        console.log("Attempting Connection...");
+        socket.onopen = () => {
+            console.log("Successfully Connected");
+            socket.send(JSON.stringify({ connectedUser: authData.username }))
+        };
+        socket.onclose = event => {
+            console.log("Socket Closed Connection: ", event);
+            socket.send("Client Closed!");
+        };
+    /*    socket.onmessage = (event) => {
+            console.log("EVENT", JSON.stringify(event.data))
+            console.log('Received message:', event.data);
+        };
 
-    /*
-   const messages = [
-        {
-            message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea error impedit vel nostrum fugiat molestias accusamus facere ipsa eveniet perspiciatis, dolor perferendis maiores, quaerat dolores architecto assumenda sed consequuntur laudantium.",
-            username: "Test",
-            date: "12-09-2024",
-            time: "12:00",
-            isRead: false,
-            id: 2,
-            logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnaEHoW5QWjxpD2klNN6aeq8eKyMjLc-Dhxg&s",
-            isGroup: false
+        */
+
+
+        socket.onmessage = (event: MessageEvent) => {
+            try {
+                const data: MessageData[] = JSON.parse(event.data);
+                setMessages(data)
+              /*  console.log("EVENT", JSON.stringify(data));
+                data.forEach(message => {
+                    console.log('Received message from:', message.from);
+                    console.log('To:', message.to);
+                    console.log('Message:', message.message);
+                    console.log('Time:', message.time);
+                }); */
+            } catch (error) {
+                console.error("Error parsing message data:", error);
+            }
+        };
+        socket.onerror = error => {
+            console.log("Socket Error: ", error);
         }
-    ]  */
+        return () => {
+            socket.close();
+        };
+    }, [isSelected]);
     useEffect(() => {  
         const token = localStorage.getItem('token')
     }, [])
@@ -39,12 +89,14 @@ const ChatWithUser = ({handleSendMessage, handleSetMessage}:  ChatWithUserProps)
                     </div>
                 ) : (
                     <div className={styles.messages}>
-                  {/*      {messages.map((item, index) => (
+
+{/*
+                        */}
+                        { messages && messages.map((item, index) => (
                             <div key={index} className={styles.message}>
-                                <img src={item.logo} alt="logo" className={styles.message__logo} />
                                 <div className={styles.message__right}>
                                     <p className={styles.message__title}>
-                                        {item.username}
+                                        {authData.username== item.from ? item.from : item.to}
                                     </p>
                                     <p className={styles.message__content}>
                                         {item.message}
@@ -56,13 +108,77 @@ const ChatWithUser = ({handleSendMessage, handleSetMessage}:  ChatWithUserProps)
                                     </div>
                                 </div>
                             </div>
-                        ))} */}
+                        ))} 
+                         {/*
+                         */}
                     </div>
                 )}
             </div>
         </div>
     );
 }
+{/*  <img src={item.logo} alt="logo" className={styles.message__logo} /> */}
 
 export default ChatWithUser;
                                     // <img src={Tick} alt="tick" className={styles.message__time__tick} />  
+
+
+                                    /*
+
+                                     const authData = useSelector(authSelector);
+    const isSelected = useSelector(selectedChatSelector)
+    const dispatch = useAppDispatch();
+    const URL_WEB_SOCKET = `ws://localhost:5000/ws?user=${authData.username}`;
+    const [ws, setWs] = useState(null);
+    const [message, setMessage] = useState("");
+    useEffect(() => {
+        let socket = new WebSocket(URL_WEB_SOCKET);
+        setWs(socket);
+        console.log("Attempting Connection...");
+        socket.onopen = () => {
+            console.log("Successfully Connected");
+            socket.send(JSON.stringify({ connectedUser: authData.username }))
+        };
+        socket.onclose = event => {
+            console.log("Socket Closed Connection: ", event);
+            socket.send("Client Closed!");
+        };
+
+        socket.onmessage = (event) => {
+          
+            console.log('Received message:', event.data);
+        };
+
+        socket.onerror = error => {
+            console.log("Socket Error: ", error);
+        }
+        return () => {
+            socket.close();
+        };
+    }, []);
+    useEffect(() => {
+        if (!localStorage.getItem('token')) {
+            dispatch(IsUnauthorizedAction({ isUnauthorized: false }));
+        }
+    }, [dispatch]);
+    useEffect(() => {
+        axios.get(`http://localhost:5000/${`chat`}/token-start?user=${authData.username}`)
+            .then(response => {
+                console.log('Ответ от сервера TOKEN START:', response.data);
+            })
+            .catch(error => {
+                console.error('Произошла ошибка при запросе:', error);
+            });
+    }, [authData.username]);
+    const handleSendMessage = () => {
+        if (ws && ws.readyState === WebSocket.OPEN) {
+            console.log("SENDDDD")
+            ws.send(JSON.stringify({ name: authData.username, message: message, to: isSelected.user }));
+            setMessage("");
+        }
+    };
+    const handleSetMessage = (value: string) => {
+        setMessage(value)
+    }
+
+    */
